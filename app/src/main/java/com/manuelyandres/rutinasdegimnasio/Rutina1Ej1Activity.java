ec5b7d23e3c6954c4952ae019dff20c5f8937f9e
B13 <manuelantoniopedrazarojas@gmail.com>
2023-10-18 08:54:43 -0500

    primeros acti

---
+++ app/src/main/java/com/manuelyandres/rutinasdegimnasio/Rutina1Ej1Activity.java
@@ -0,0 +1,14 @@
+package com.manuelyandres.rutinasdegimnasio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rutina1Ej1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rutina1_ej1);
+    }
+}
