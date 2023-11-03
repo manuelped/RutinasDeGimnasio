1ca3736177342b14980891af7efb2a4c9b302e2c
B13 <manuelantoniopedrazarojas@gmail.com>
2023-11-03 09:31:22 -0500

    casi todos los activies terminados

--- app/src/main/java/com/manuelyandres/rutinasdegimnasio/Rutina1Ej1Activity.java
+++ app/src/main/java/com/manuelyandres/rutinasdegimnasio/Rutina1Ej1Activity.java
@@ -3,12 +3,19 @@ package com.manuelyandres.rutinasdegimnasio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Rutina1Ej1Activity extends AppCompatActivity {
+
+    private Button btnAtras,btnVolverinicio,btnAdelante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rutina1_ej1);
+        btnAtras = findViewById(R.id.btnatra);
+        btnVolverinicio = findViewById(R.id.btnvolvinicio);
+        btnAdelante = findViewById(R.id.btnadel);
     }
 }
