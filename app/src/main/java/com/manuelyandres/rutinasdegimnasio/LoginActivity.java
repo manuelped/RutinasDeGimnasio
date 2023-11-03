922dbffaa2fd43b0361b0fa0b45c25089006471b
B13 <manuelantoniopedrazarojas@gmail.com>
2023-11-03 09:22:04 -0500

    casi todos los activies terminados

--- app/src/main/java/com/manuelyandres/rutinasdegimnasio/LoginActivity.java
+++ app/src/main/java/com/manuelyandres/rutinasdegimnasio/LoginActivity.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnRegister;
+    private Button btnLogin;
+    private TextInputEditText etusuariolo, etpasswlo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        etusuariolo = findViewById(R.id.inputTextUsernamelogin);
+        etpasswlo = findViewById(R.id.inputTextPasswordlogin);
+        btnLogin = findViewById(R.id.buttonLogin);
         btnRegister = findViewById(R.id.buttonRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/manuelyandres/rutinasdegimnasio/RegisterActivity.java
+++ app/src/main/java/com/manuelyandres/rutinasdegimnasio/RegisterActivity.java
@@ -6,19 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Button btnConfirm,btnCancel;
-    private TextInputEditText etNameAccount, etTypeAccount, etBalanceInitial;
+    private TextInputEditText etusuariore, etpasswre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         btnCancel = findViewById(R.id.buttonCancel);
         btnConfirm = findViewById(R.id.buttonConfirm);
+        etusuariore = findViewById(R.id.inputTextUsernameregister);
+        etpasswre = findViewById(R.id.inputTextPasswordregister);
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
