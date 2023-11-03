2ff859b63ee45342c000cfa875941ccffedd44f6
B13 <manuelantoniopedrazarojas@gmail.com>
2023-11-03 09:00:13 -0500

    codigo

--- app/src/main/java/com/manuelyandres/rutinasdegimnasio/RegisterActivity.java
+++ app/src/main/java/com/manuelyandres/rutinasdegimnasio/RegisterActivity.java
@@ -2,13 +2,29 @@ package com.manuelyandres.rutinasdegimnasio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button btnConfirm,btnCancel;
+    private TextInputEditText etNameAccount, etTypeAccount, etBalanceInitial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btnCancel = findViewById(R.id.buttonCancel);
+        btnConfirm = findViewById(R.id.buttonConfirm);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
